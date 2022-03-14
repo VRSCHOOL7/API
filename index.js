3dae84b499704069f71886212ac3579d96058c5d
Mykhailo Yakyma <mexayakima1@gmail.com>
2022-03-14 11:59:06 +0100

    'fixes'

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const express = require('express'); // Importar express
-const app = express(); // Asignar express a la variable app
-const port = 8000; // Puerto para express
+const express = require('express')
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+// const StudentModel = require('./models/students')
+// const TeacherModel = require('./models/teachers')
+const PORT = process.env.PORT || 5000
+// const test = process.env;
+
+
+// mongoose.connect()
+
+// app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
+
+
+app.listen(port, () => (
+  console.log(path)
+));
+
 
 app.get('/', (req, res) => {
     // Información recibida desde el explorador
     let student = req.query.student;
 
     // Array con los estudiantes
-    let students = ["hola mundo", "Student2"];
+    let students = ["Student1", "Student2"];
 
     // Respuesta base
     let page = '<label>Buscador de Alumnos</label><input type="text" id="student"></input><input type="button" onclick="checkStudent()" value="Buscar"></input>';
@@ -25,7 +50,20 @@ app.get('/', (req, res) => {
     }
 });
 
-// Crear un servidor web con express el el puerto asignado en la varible port.
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.get('/login', (req, res) => {
+
 });
+
+app.get('/logout', (req, res) => {
+
+});
+
+app.get('/get_course', (req, res) => {
+
+});
+
+app.get('/get_course_details', (req, res) => {
+
+});
+
+

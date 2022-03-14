11893d0404c8d4a007c87fd9a0b6b8a02bc16160
Mykhailo Yakyma <mexayakima1@gmail.com>
2022-03-14 12:12:22 +0100

    'fixes'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 //const mongoose = require('mongoose');
 // const StudentModel = require('./models/students')
 // const TeacherModel = require('./models/teachers')
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 7000
 // const test = process.env;
 
 
@@ -23,31 +23,34 @@ const PORT = process.env.PORT || 5000
 
 
 app.listen(port, () => (
-  console.log(path)
+  console.log(port)
 ));
 
 
 app.get('/', (req, res) => {
-    // Información recibida desde el explorador
-    let student = req.query.student;
+    // // Información recibida desde el explorador
+    // let student = req.query.student;
 
-    // Array con los estudiantes
-    let students = ["Student1", "Student2"];
+    // // Array con los estudiantes
+    // let students = ["Student1", "Student2"];
 
-    // Respuesta base
-    let page = '<label>Buscador de Alumnos</label><input type="text" id="student"></input><input type="button" onclick="checkStudent()" value="Buscar"></input>';
+    // // Respuesta base
+    // let page = '<label>Buscador de Alumnos</label><input type="text" id="student"></input><input type="button" onclick="checkStudent()" value="Buscar"></input>';
 
-    // Añadir JS de redirección a la respuesta base
-    page += '<script>function checkStudent(){if (document.getElementById("student").value != "") {window.location.replace(window.location.origin + "/?student=" + document.getElementById("student").value)}else{window.location.replace(window.location.origin)}}</script>';
+    // // Añadir JS de redirección a la respuesta base
+    // page += '<script>function checkStudent(){if (document.getElementById("student").value != "") {window.location.replace(window.location.origin + "/?student=" + document.getElementById("student").value)}else{window.location.replace(window.location.origin)}}</script>';
 
-    // Dependiendo de si la variable student está definida y si se encuentra en students, mostrar el contenido correspondiente.
-    if (students.includes(student)){
-        res.send(page + `<p>Bienvenido ${student}</p>`);
-    } else if (typeof(student) != 'undefined'){
-        res.send(page + `<p>Estudiante ${student} no encontrado</p>`);
-    } else {
-        res.send(page);
-    }
+    // // Dependiendo de si la variable student está definida y si se encuentra en students, mostrar el contenido correspondiente.
+    // if (students.includes(student)){
+    //     res.send(page + `<p>Bienvenido ${student}</p>`);
+    // } else if (typeof(student) != 'undefined'){
+    //     res.send(page + `<p>Estudiante ${student} no encontrado</p>`);
+    // } else {
+    //     res.send(page);
+    // }
+
+    res.send("<h1>hello world</h1>");
+
 });
 
 app.get('/login', (req, res) => {

2185675bfd033f2ea8339c6b19c5e913c3a65db7
Mykhailo Yakyma <mexayakima1@gmail.com>
2022-03-15 19:16:36 +0100

    dg>

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ var database, courses, users;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(port, () => {
   console.log(database_url);
